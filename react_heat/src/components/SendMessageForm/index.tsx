9bebc5e72e9cf9797a85f29553904d6b6aea343f
Lucas Souza <lucasp.sdev@gmail.com>
2021-10-20 15:08:34 -0300

    feat: add toast when sending message

--- react_heat/src/components/SendMessageForm/index.tsx
+++ react_heat/src/components/SendMessageForm/index.tsx
@@ -1,4 +1,5 @@
 import { useContext, useState, FormEvent } from 'react'
+import { toast, Toaster } from 'react-hot-toast'
 import { VscGithubInverted, VscSignOut } from 'react-icons/vsc'
 import { AuthContext } from '../../contexts/auth'
 import { api } from '../../services/api'
@@ -12,43 +13,48 @@ export function SendMessageForm(){
     event.preventDefault()
 
     if(!message.trim()) {
+      toast.error("Impossível enviar mensagens vazias")
       return
     }
 
     await api.post('messages', { message })
+    toast.success('Mensagem enviada com sucesso!')
 
     setMessage('')
   }
 
   return (
-    <div className={styles.sendMessageFormWrapper}>
-      <button onClick={signOut} className={styles.signOutButton}>
-        <VscSignOut size={"32"} />
-      </button>
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+      <div className={styles.sendMessageFormWrapper}>
+        <button onClick={signOut} className={styles.signOutButton}>
+          <VscSignOut size={"32"} />
+        </button>
 
-      <header className={styles.userInformation}>
-        <div className={styles.userImage}>
-          <img src={user?.avatar_url} alt={user?.name} />
-        </div>
-        <strong className={styles.userName}>{user?.name}</strong>
-        <span className={styles.userGitHub}>
-          <VscGithubInverted size={"16"} />
-          {user?.login}
-        </span>
-      </header>
+        <header className={styles.userInformation}>
+          <div className={styles.userImage}>
+            <img src={user?.avatar_url} alt={user?.name} />
+          </div>
+          <strong className={styles.userName}>{user?.name}</strong>
+          <span className={styles.userGitHub}>
+            <VscGithubInverted size={"16"} />
+            {user?.login}
+          </span>
+        </header>
 
-      <form onSubmit={handleSendMessage} className={styles.sendMessageForm}>
-        <label htmlFor="message">Mensagem</label>
-        <textarea
-          name="message"
-          id="message"
-          placeholder='Qual sua expectativa para o evento ?'
-          onChange={event => setMessage(event.target.value)}
-          value={message}
+        <form onSubmit={handleSendMessage} className={styles.sendMessageForm}>
+          <label htmlFor="message">Mensagem</label>
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Qual sua expectativa para o evento ?"
+            onChange={(event) => setMessage(event.target.value)}
+            value={message}
           />
 
-          <button type='submit'>Enviar Mensagem</button>
-      </form>
-    </div>
-  )
+          <button type="submit">Enviar Mensagem</button>
+        </form>
+      </div>
+    </>
+  );
 }
